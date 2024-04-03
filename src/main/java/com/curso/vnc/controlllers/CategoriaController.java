1858b4a88bd23cfb7eb58b876822acde026544ac
Vinicius Vieira Carneiro <vinicius.95vieira00@gmail.com>
2024-04-03 11:09:52 -0300

    post categoria

--- src/main/java/com/curso/vnc/controlllers/CategoriaController.java
+++ src/main/java/com/curso/vnc/controlllers/CategoriaController.java
@@ -3,14 +3,19 @@ package com.curso.vnc.controlllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.curso.vnc.domain.Categoria;
+import com.curso.vnc.domain.dto.CategoriaDto;
 import com.curso.vnc.services.CategoriaService;
 
 @RestController
@@ -32,8 +37,14 @@ public class CategoriaController {
 		return ResponseEntity.ok().body(cat);
 	}
 	
+	@PostMapping
+	public ResponseEntity<CategoriaDto> inserir(@RequestBody CategoriaDto catDto) {
+		var categoria = service.inserir(catDto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoria);
+	}
+
 	@DeleteMapping(value = "/delete/{id}")
-	public ResponseEntity<?> delete(@PathVariable Integer id){
+	public ResponseEntity<?> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.ok().build();
 	}
